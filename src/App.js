65532a356908ab835a6deb0aae040a142c5820fd
System Administrator <root@Ashishs-MacBook-Air.local>
2020-10-24 02:09:59 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Format Number Function
+const formatNumber = (number) => new Intl.NumberFormat("en", { minimumFractionDigits: 2 }).format(number);
+
+class App extends Component {
+
+    state = {
+        products: null,
+        filter: ''
+    }
+
+    inputFilterHandler = (event) => {
+        this.setState({ filter: event.target.value });
+    }
+
+    getAggregatedProducts(products, filter) {
+        const totalProducts = {};
+
+        products.forEach((item) => {
+            if (filter && new RegExp(filter, 'i').test(item.name) === false) {
+                return;
+            }
+
+            if (!totalProducts[item.id]) {
+                totalProducts[item.id] = { ...item }
+            } else {
+                totalProducts[item.id].sold += item.sold;
+            }
+        });
+
+        const totalProductsArray = Object.values(totalProducts);
+
+        totalProductsArray.sort((a, b) => a.name.toUpperCase().localeCompare(b.name.toUpperCase()));
+
+        return totalProductsArray;
+    }
+
+    // Fetch Branch Data Function
+    fetchBranchData = (branchName) => {
+        return fetch(`api/${branchName}.json`).then(response => response.json());
+    }
+
+    componentDidMount() {
+        Promise.all([
+            this.fetchBranchData('branch1'),
+            this.fetchBranchData('branch2'),
+            this.fetchBranchData('branch3')
+        ]).then(data => {
+            this.setState({ products: [...data[0].products, ...data[1].products, ...data[2].products] });
+        }).catch(err => console.log('Fetch Error Message: ', err));
+    }
+
+    render() {
+        if (!this.state.products) {
+            return 'Loading...';
+        }
+        const aggregatedProducts = this.getAggregatedProducts(this.state.products, this.state.filter);
+        const total = aggregatedProducts.reduce((total, item) => total + (item.sold * item.unitPrice), 0);
+        const table = aggregatedProducts.map(list => {
+            return (
+                <tr key={list.name}>
+                    <td>{list.name}</td>
+                    <td>{formatNumber(list.sold * list.unitPrice)}</td>
+                </tr>
+            )
+        });
+
+        return (
+            <main className="product-list">
+                <label>Search Products</label>
+                <input type="text" onChange={this.inputFilterHandler} />
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Product</th>
+                            <th>Revenue</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {table}
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td>Total</td>
+                            <td>{formatNumber(total)}</td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </main>
+        );
+    }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,87 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import Adapter from "enzyme-adapter-react-16";
+import { configure, shallow } from "enzyme";
+import "jest-enzyme";
+import branch1 from "../public/api/branch1.json";
+import branch2 from "../public/api/branch2.json";
+import branch3 from "../public/api/branch3.json";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({
+    adapter: new Adapter()
+});
+
+const responses = {
+    "api/branch1.json": branch1,
+    "api/branch2.json": branch2,
+    "api/branch3.json": branch3,
+    "/api/branch1.json": branch1,
+    "/api/branch2.json": branch2,
+    "/api/branch3.json": branch3
+};
+
+global.fetch = endpoint =>
+    Promise.resolve({
+        json: () => Promise.resolve(responses[endpoint])
+    });
+
+const flushAllPromises = () => new Promise(resolve => setImmediate(resolve));
+
+export const flushRequestsAndUpdate = async enzymeWrapper => {
+    await flushAllPromises();
+    enzymeWrapper.update();
+};
+
+it("renders without crashing", () => {
+    shallow(<App />);
+});
+
+it("renders loading text initially", async () => {
+    const app = shallow(<App />);
+    expect(app).toHaveText("Loading...");
+});
+
+it("renders a table after data load", async () => {
+    const app = shallow(<App />);
+    expect(app).toHaveText("Loading...");
+    await flushRequestsAndUpdate(app);
+    expect(app.find("table")).toExist();
+});
+
+it("renders rows with country name as key", async () => {
+    const app = shallow(<App />);
+    await flushRequestsAndUpdate(app);
+
+    expect(
+        app
+            .find("table tbody tr")
+            .at(56)
+            .key()
+    ).toEqual("Hominy");
+    expect(
+        app
+            .find("table tbody tr")
+            .at(73)
+            .key()
+    ).toEqual("Lychee");
+});
+
+it("renders table that is sorted ascending", async () => {
+    const app = shallow(<App />);
+    await flushRequestsAndUpdate(app);
+    expect(app.find("table")).toMatchSnapshot();
+});
+
+it("calculates total revenue of all branches", async () => {
+    const app = shallow(<App />);
+    await flushRequestsAndUpdate(app);
+    expect(app.find("tfoot td:last-child").text()).toEqual("2,102,619.44");
+});
+
+it("filters the displayed products", async () => {
+    const app = shallow(<App />);
+    await flushRequestsAndUpdate(app);
+    const changeEvent = { target: { value: "pear" } };
+    app.find("input").simulate("change", changeEvent);
+    expect(app.find("tfoot td:last-child").text()).toEqual("60,681.02");
 });
